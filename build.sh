ae75259653fae94970ab425c542424c1049c2063
elecburger <info@electronicburger.com>
2021-10-18 15:43:33 +0200

    Fixes in build.sh

--- build.sh
+++ build.sh
@@ -4,33 +4,34 @@ RPR_CONFIG_DIR=ME_ReaperStarterPack
 RPR_INI_DIR=ini
 VERSION=$( cat info/version.txt )
 
-mkdir -p build/tmp
+mkdir -p build/test
 mkdir -p build/release
 mkdir -p build/test
+mkdir -p add_to_test
 
 # Copy base dir
-rm -rf build/tmp/$RPR_CONFIG_DIR
-cp -r $RPR_CONFIG_DIR build/tmp
+rm -rf build/test/$RPR_CONFIG_DIR
+cp -r $RPR_CONFIG_DIR build/test
 
 # TODO Create ReaperConfigZip here
 
 # Create .ini files from Reaper import/export files
-cp $RPR_CONFIG_DIR/KeyMaps/ImportScripts_MasteringExplained_StarterPack.ReaperKeyMap build/tmp/$RPR_CONFIG_DIR/reaper-kb.ini
-cp $RPR_CONFIG_DIR/MenuSets/Toolbar_MasteringExplained_StarterPack.ReaperMenu build/tmp/$RPR_CONFIG_DIR/reaper-menu.ini
-cp $RPR_CONFIG_DIR/FXChains/Monitor_FX-Chain_ME_StarterPack.RfxChain build/tmp/$RPR_CONFIG_DIR/reaper-hwoutfx.ini
-cp $RPR_CONFIG_DIR/MouseMaps/MouseModifiers_ME_StarterPack.ReaperMouseMap build/tmp/$RPR_CONFIG_DIR/reaper-mouse.ini
+cp $RPR_CONFIG_DIR/KeyMaps/ImportScripts_MasteringExplained_StarterPack.ReaperKeyMap build/test/$RPR_CONFIG_DIR/reaper-kb.ini
+cp $RPR_CONFIG_DIR/MenuSets/Toolbar_MasteringExplained_StarterPack.ReaperMenu build/test/$RPR_CONFIG_DIR/reaper-menu.ini
+cp $RPR_CONFIG_DIR/FXChains/Monitor_FX-Chain_ME_StarterPack.RfxChain build/test/$RPR_CONFIG_DIR/reaper-hwoutfx.ini
+cp $RPR_CONFIG_DIR/MouseMaps/MouseModifiers_ME_StarterPack.ReaperMouseMap build/test/$RPR_CONFIG_DIR/reaper-mouse.ini
 
 # Copy .ini files
-cp $RPR_INI_DIR/reaper.ini build/tmp/$RPR_CONFIG_DIR/reaper.ini
+cp $RPR_INI_DIR/reaper.ini build/test/$RPR_CONFIG_DIR/reaper.ini
 
 # Copy license, version, whatsnew and about files
-cp LICENSE build/tmp/$RPR_CONFIG_DIR/Scripts/MasteringExplained_StarterPack/lib/LICENSE
-cp info/version.txt build/tmp/$RPR_CONFIG_DIR/Scripts/MasteringExplained_StarterPack/lib/version.txt
-cp info/whatsnew.txt build/tmp/$RPR_CONFIG_DIR/Scripts/MasteringExplained_StarterPack/lib/whatsnew.txt
-cp info/about.txt build/tmp/$RPR_CONFIG_DIR/Scripts/MasteringExplained_StarterPack/lib/about.txt
+cp LICENSE build/test/$RPR_CONFIG_DIR/Scripts/MasteringExplained_StarterPack/lib/LICENSE
+cp info/version.txt build/test/$RPR_CONFIG_DIR/Scripts/MasteringExplained_StarterPack/lib/version.txt
+cp info/whatsnew.txt build/test/$RPR_CONFIG_DIR/Scripts/MasteringExplained_StarterPack/lib/whatsnew.txt
+cp info/about.txt build/test/$RPR_CONFIG_DIR/Scripts/MasteringExplained_StarterPack/lib/about.txt
 
 # Create zip-file
-cd build/tmp/
+cd build/test/
 # ZIPNAME=StarterPack-$VERSION.ReaperConfigZip
 ZIPNAME=$RPR_CONFIG_DIR-$VERSION.zip
 rm ../release/$ZIPNAME
@@ -38,5 +39,5 @@ zip -r ../release/$ZIPNAME $RPR_CONFIG_DIR --no-dir-entries -x "**/.DS_Store" -x
 cd ../..
 
 # Create test install
-cp -R add_to_test/* build/tmp/$RPR_CONFIG_DIR
-mv build/tmp/$RPR_CONFIG_DIR build/tmp/$RPR_CONFIG_DIR-$VERSION
+cp -R add_to_test/* build/test/$RPR_CONFIG_DIR
+mv build/test/$RPR_CONFIG_DIR build/test/$RPR_CONFIG_DIR-$VERSION
